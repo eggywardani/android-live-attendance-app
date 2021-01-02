cb1f26d95826c54ab5148116101359efee30a30a
eggywardani <eggyandikawardani@gmail.com>
2021-01-02 13:23:31 +0700

    ui: add login page

---
+++ app/src/main/java/com/eggy/liveattendanceapp/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.eggy.liveattendanceapp.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eggy.liveattendanceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
