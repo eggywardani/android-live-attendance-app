7299091933e88eef0150e5d33caf688994ef2935
eggywardani <eggyandikawardani@gmail.com>
2021-01-02 17:28:56 +0700

    ui: add ui attendance

--- app/src/main/java/com/eggy/liveattendanceapp/views/splash/SplashActivity.kt
+++ app/src/main/java/com/eggy/liveattendanceapp/views/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.eggy.liveattendanceapp.views.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eggy.liveattendanceapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
